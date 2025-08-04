515efed2b1f8eb6a608d055b5bd14ff17b407264
dolf <to.dolfin@gmail.com>
2025-08-04 22:57:02 +0500

    code copy final!

--- src/main/kotlin/io/qent/sona/ui/CopyableCodeBlock.kt
+++ src/main/kotlin/io/qent/sona/ui/CopyableCodeBlock.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
@@ -23,14 +25,22 @@ import org.jetbrains.jewel.ui.component.IconButton
 @Composable
 fun CopyableCodeBlock(model: MarkdownComponentModel, fence: Boolean) {
     val clipboard = LocalClipboardManager.current
+    val extractedCode = remember { mutableStateOf("") }
+
     Box {
         if (fence) {
-            MarkdownCodeFence(model.content, model.node, style = model.typography.code)
+            MarkdownCodeFence(model.content, model.node, style = model.typography.code) { code, _, _ ->
+                extractedCode.value = code
+                MarkdownCodeFence(model.content, model.node, style = model.typography.code)
+            }
         } else {
-            MarkdownCodeBlock(model.content, model.node, style = model.typography.code)
+            MarkdownCodeBlock(model.content, model.node, style = model.typography.code) { code, _, _ ->
+                extractedCode.value = code
+                MarkdownCodeBlock(model.content, model.node, style = model.typography.code)
+            }
         }
         IconButton(onClick = {
-            clipboard.setText(AnnotatedString(model.node.getTextInNode(model.content).toString())
+            clipboard.setText(AnnotatedString(extractedCode.value)
         ) }, modifier = Modifier.align(Alignment.TopEnd).padding(top = 12.dp, end = 4.dp)) {
             Image(
                 painter = loadIcon("/icons/copy.svg"),
