1aeff790aacc0e84b1ba90cee669d7923ca3a1ec
Alexey Emelin <to.dolfin@gmail.com>
2025-08-05 00:15:35 +0500

    Use Gson for project configuration

--- src/main/kotlin/io/qent/sona/repositories/PluginFilePermissionsRepository.kt
+++ src/main/kotlin/io/qent/sona/repositories/PluginFilePermissionsRepository.kt
@@ -1,12 +1,15 @@
 package io.qent.sona.repositories
 
 import com.intellij.openapi.project.Project
+import io.qent.sona.config.SonaConfig
 import io.qent.sona.core.FilePermissionsRepository
 
 class PluginFilePermissionsRepository(project: Project) : FilePermissionsRepository {
     private val root = project.basePath ?: "/"
-    override val whitelist = listOf("$root/.*")
-    override val blacklist = listOf(
+    private val config = SonaConfig.load(root)
+
+    override val whitelist = config?.permissions?.files?.whitelist ?: listOf("$root/.*")
+    override val blacklist = config?.permissions?.files?.blacklist ?: listOf(
         ".*/\\.env.*",
         ".*/\\.git.*",
         ".*/\\.idea/.*",
