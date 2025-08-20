2b544ce407a1c3f778d63fdf809c386c2b3db285
Alexey Emelin <to.dolfin@gmail.com>
2025-08-20 23:55:14 +0500

    Parse search results from JSON

--- core/src/main/kotlin/io/qent/sona/core/model/SearchAiService.kt
+++ core/src/main/kotlin/io/qent/sona/core/model/SearchAiService.kt
@@ -1,10 +1,9 @@
 package io.qent.sona.core.model
 
-import dev.langchain4j.model.chat.response.ChatResponse
 import dev.langchain4j.service.UserMessage
 
 interface SearchAiService {
     fun chat(
         @UserMessage message: String
-    ): ChatResponse
+    ): String
 }
